fd3ed7db325d95a14b26039b8a6ba4819c3a282e
GabrielSchauren <gajschauren@minha.fag.edu.br>
2026-06-22 19:47:49 -0300

    configurando para o netlify.

--- greenlife/src/App.tsx
+++ greenlife/src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Solutions from './components/Solutions';
-import ProductGrid from './components/ProductGrid';
-import Testimonials from './components/Testimonials';
-import Pricing from './components/Pricing';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './App.css';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Solutions from "./components/Solutions";
+import ProductGrid from "./components/ProductGrid";
+import Testimonials from "./components/Testimonials";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
